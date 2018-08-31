672dfd026e42094d6a96295261ff3c53f3663993
annagashina <anna.gashina@flatstack.com>
2018-08-31 09:13:26 +0300

    try JS

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+var firstVar = "6";
+var secondVar = 8;
+var resultVar = (firstVar * secondVar);
+console.log(resultVar);
+var textResultVar = firstVar + " * " + secondVar + " = " + resultVar;
+console.log(textResultVar);
+var textResultVar2 = "${firstVar}";
+console.log(textResultVar2);
