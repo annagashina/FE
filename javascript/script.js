17a4af6967cd0d5b509e3121e95bd1ca5e32cc14
annagashina <anna.gashina@flatstack.com>
2018-09-04 09:13:22 +0300

    edit script

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,10 @@ var textResultVar = firstVar + " * " + secondVar + " = " + resultVar;
 console.log(textResultVar);
 var textResultVar2 = "${firstVar}";
 console.log(textResultVar2);
+
+var oneVar = 5;
+var twoVar = 6;
+var threeVar = 7;
+var resultVarOne = ((oneVar + twoVar)* threeVar);
+var textResultVarOne = "(" + oneVar + "+" + twoVar + ")" + " * " + threeVar + " = " + resultVarOne;
+console.log(textResultVarOne);
